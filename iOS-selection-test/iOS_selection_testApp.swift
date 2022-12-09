354d2ad7b70eaccaa1dde95f9e1607b545cdc498
Igor Squadra <igor@igorsquadra.com>
2022-12-09 10:13:16 +0100

    Initial Commit

---
+++ iOS-selection-test/iOS_selection_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_selection_testApp.swift
+//  iOS-selection-test
+//
+//  Created by Igor Squadra on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_selection_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
